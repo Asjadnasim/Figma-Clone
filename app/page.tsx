e26257fd49bb2926e22eca5d232cce99baab409d
Asjad Nasim <asjadnasim@gmail.com>
2024-02-22 17:14:29 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div className='h-[100vh] w-full flex items-center justify-center text-center'>
+			<h1 className='text-2xl text-white'>LiveBlocks Figma Clone</h1>;
+		</div>
+	);
+}
