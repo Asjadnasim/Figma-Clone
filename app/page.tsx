1774c39e82e7aa43ad1631750ebe970cad03f296
Asjad Nasim <asjadnasim@gmail.com>
2024-02-28 21:47:57 +0530

    Images

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ import { ActiveElement } from '@/types/type';
 import { useMutation, useRedo, useStorage, useUndo } from '@/liveblocks.config';
 import { defaultNavElement, navElements } from '@/constants';
 import { handleDelete, handleKeyDown } from '@/lib/key-events';
+import { handleImageUpload } from '@/lib/shapes';
 
 export default function Home() {
 	const undo = useUndo();
@@ -31,6 +32,7 @@ export default function Home() {
 	const shapeRef = useRef<fabric.Object | null>(null);
 	const selectedShapeRef = useRef<string | null>(null);
 	const activeObjectRef = useRef<fabric.Object | null>(null);
+	const imageInputRef = useRef<HTMLInputElement>(null);
 
 	const canvasObjects = useStorage((root) => root.canvasObjects);
 
@@ -84,6 +86,16 @@ export default function Home() {
 			case 'delete':
 				handleDelete(fabricRef.current as any, deleteShapeFromStorage);
 				setActiveElement(defaultNavElement);
+				break;
+
+			case 'image':
+				imageInputRef.current?.click();
+				isDrawing.current = false;
+
+				if (fabricRef.current) {
+					fabricRef.current.isDrawingMode = false;
+				}
+				break;
 
 			default:
 				break;
@@ -168,6 +180,17 @@ export default function Home() {
 			<Navbar
 				activeElement={activeElement}
 				handleActiveElement={handleActiveElement}
+				imageInputRef={imageInputRef}
+				handleImageUpload={(e: any) => {
+					e.stopPropagation();
+
+					handleImageUpload({
+						file: e.target.files[0],
+						canvas: fabricRef as any,
+						shapeRef,
+						syncShapeInStorage,
+					});
+				}}
 			/>
 
 			<section className='flex h-full flex-row'>
